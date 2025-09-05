03100b7fc1dcd49ef5876a96f0e459ae117f9ec3
Soumya Mp <sowmyampetkar@gmail.com>
2025-09-05 08:00:58 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -1,28 +1,20 @@
 from flask import Flask, request, render_template
+from pyexpat.errors import messages
 
-app1=Flask(__name__)
-# app.run(debug=False)
+app=Flask(__name__)
+@app.route("/feedback",methods=["POST","GET"])
+def feedback():
+    if request.method==("POST"):
+        name=request.form.get("username")
+        message=request.form.get("message")
 
-#@app.route("/")#This line is called a route decorator in Flask.
-# It tells Flask which URL should trigger which function.
-#@app1.route('/')
-@app1.route('/home')
-def home():
-    return "hello user!this is  my first flask app"
+        return render_template("thankyou.html",
+                               user=name,message=message)
 
-@app1.route('/about')
-def about():
-    return "this  is about us page"
-
-@app1.route('/contact')
-def contact():
-    return "this  is a contact us page"
-@app1.route('/submit',methods=['GET','POST'])
-def submit():
-    if request.method=='POST':
-           return "you send data"
-    else:
-           return "your are only viewing"
+    return render_template("feedback.html")
 
 if __name__ == "__main__":
-    app1.run(debug=True)
+    app.run(debug=True)
+
+
+
