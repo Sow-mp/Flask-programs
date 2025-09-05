197f9954e085ccf3c45230478a8940546d60dce4
Soumya Mp <sowmyampetkar@gmail.com>
2025-09-05 08:02:13 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -1,20 +1,48 @@
+import uuid
+import logging
 from flask import Flask, request, render_template
-from pyexpat.errors import messages
+from flask_sessionstore import Session
+from flask_session_captcha import FlaskSessionCaptcha
+from pymongo import MongoClient
 
-app=Flask(__name__)
-@app.route("/feedback",methods=["POST","GET"])
-def feedback():
-    if request.method==("POST"):
-        name=request.form.get("username")
-        message=request.form.get("message")
+app = Flask(__name__)
 
-        return render_template("thankyou.html",
-                               user=name,message=message)
+# Database Config
+# If your mongodb runs on a different port
+# change 27017 to that port number
+mongoClient = MongoClient('localhost', 27017)
 
-    return render_template("feedback.html")
+# Captcha Configuration
+app.config["SECRET_KEY"] = uuid.uuid4()
+app.config['CAPTCHA_ENABLE'] = True
+
+# Set 5 as character length in captcha
+app.config['CAPTCHA_LENGTH'] = 5
+
+# Set the captcha height and width
+app.config['CAPTCHA_WIDTH'] = 160
+app.config['CAPTCHA_HEIGHT'] = 60
+app.config['SESSION_MONGODB'] = mongoClient
+app.config['SESSION_TYPE'] = 'mongodb'
+
+# Enables server session
+Session(app)
+
+# Initialize FlaskSessionCaptcha
+captcha = FlaskSessionCaptcha(app)
+
+@app.route('/', methods=['POST', 'GET'])
+def index():
+    if request.method == "POST":
+        if captcha.validate():
+            return "success"
+        else:
+            return "fail"
+
+    return render_template("form.html")
+
 
 if __name__ == "__main__":
-    app.run(debug=True)
-
-
-
+    app.debug = True
+    logging.getLogger().setLevel("DEBUG")
+    app.run()
